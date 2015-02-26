6b1e3a2fe43399ffbadc7259f355b57be52a4b99
Elizabeth <ebrayton@gmail.com>
2015-02-26 00:49:08 -0700

    Added transition to work details page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,31 @@
 $( document ).ready(function() {
-
-  //
-
+	smoothScroll(300);
+	workBelt();
 });
+
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+function workBelt () {
+	$('.thumb-unit').click(function() {
+		$('.work-belt').css('left', '-100%')
+		$('.work-container').show();
+	});
+
+	$('.work-return').click(function() {
+		$('.work-belt').css('left', '0%')
+		$('.work-container').hide(800);
+	});
+}
